09e07492c68a643c60335a1ef0ed3f43874f3893
Shravanipande02 <pandeshravani02@gmail.com>
2026-04-15 12:48:32 +0530

    Added __init__.py file

---
+++ emotion_detection.py
@@ -0,0 +1,31 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    """
+    This function sends text to Watson NLP API and returns emotion scores.
+    """
+    
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+    
+    response = requests.post(url, json=input_json, headers=headers)
+    
+    if response.status_code == 200:
+        formatted_response = response.json()
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        return emotions
+    
+    elif response.status_code == 400:
+        return None
+    
+    else:
+        return None
